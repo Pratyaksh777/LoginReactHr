dcc9e637b9d3da3dc076ee5dcded941bba7038c3
Cramiac <66597883+Cramiac@users.noreply.github.com>
2020-06-11 15:27:37 +0530

    homepage.js
    bootstrap check

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState} from 'react';
 import {Redirect} from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import { Button } from 'reactstrap';
 import FetchComp from "./components/FetchComp"
 import LogOut from './components/LogOut';
 import Delete from './components/Delete';
